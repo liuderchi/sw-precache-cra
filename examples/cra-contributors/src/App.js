44a6d2b8ffec196e36fd4bc1062c2e548f56d486
liuderchi <liuderchi@gmail.com>
2018-06-11 15:35:22 +0800

    [ step 2 ] 👷 fetch contributors with gh api

--- examples/cra-contributors/src/App.js
+++ examples/cra-contributors/src/App.js
@@ -3,16 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    contributors: [],
+  };
+  componentDidMount() {
+    fetch('https://api.github.com/repos/facebook/create-react-app/contributors')
+      .then(res => res.json())
+      .then(contributors => this.setState({ contributors }));
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Contributors of Create React App</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h3>👷 Service worker caches API response. This page works offline!</h3>
+        <div className="App-contributors">
+          {this.state.contributors.map(({ id, avatar_url, login, url }) => (
+            <div key={id} className="App-contributor">
+              <img src={avatar_url} width={50} />
+              <div>
+                <a href={url}>
+                  <code>{login}</code>
+                </a>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
